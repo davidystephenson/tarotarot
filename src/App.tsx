6f1a7544b41726e2920398069c4223c83571488c
David Y. Stephenson <david@davidystephenson.com>
2025-08-24 10:30:03 -0400

    Display grid and table

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import cards from './cards.json'
 import { useState } from 'react'
 
-export default function App () {
+export default function App() {
   const [query, setQuery] = useState('')
   const [tag, setTag] = useState('')
+  const [selectedNames, setSelectedNames] = useState<string[]>([])
   const tags = cards.flatMap(card => card.tags)
   const tagSet = new Set(tags)
   const uniqueTags = Array.from(tagSet).sort()
@@ -38,19 +39,93 @@ export default function App () {
     return lowerTags.some(tag => tag.includes(lowerQuery))
   })
   const rows = filtered.map(card => {
+    function handleSelect() {
+      const newSelectedNames = [...selectedNames, card.name]
+      setSelectedNames(newSelectedNames)
+    }
+    const selected = selectedNames.includes(card.name)
+    const key = `${card.name}-all`
     return (
-      <tr>
+      <tr key={key}>
         <td>{card.designation}</td>
         <td>{card.name}</td>
         <td>{card.upright}</td>
         <td>{card.reversed}</td>
         <td>{card.tags.join(', ')}</td>
+        <td>
+          <img src={card.image} />
+        </td>
+        <td>
+          {!selected && (
+            <button onClick={handleSelect}>Select</button>
+          )}
+        </td>
       </tr>
+    )
+  })
+  const selectedRows = selectedNames.map((name) => {
+    const card = cards.find(card => card.name === name)
+    if (!card) {
+      throw new Error(`Card ${name} not found`)
+    }
+    function handleUnselect() {
+      const newSelectedNames = selectedNames.filter(selectedName => selectedName !== name)
+      console.log('newSelectedNames', newSelectedNames)
+      setSelectedNames(newSelectedNames)
+    }
+    const key = `${card.name}-selected`
+    return (
+      <tr key={key}>
+        <td>{card.designation}</td>
+        <td>{card.name}</td>
+        <td>{card.upright}</td>
+        <td>{card.reversed}</td>
+        <td>{card.tags.join(', ')}</td>
+        <td>
+          <button onClick={handleUnselect}>Unselect</button>
+        </td>
+      </tr>
+    )
+  })
+  const gridItems = filtered.map(card=> {
+    function handleSelect() {
+      const newSelectedNames = [...selectedNames, card.name]
+      setSelectedNames(newSelectedNames)
+    }
+    const selected = selectedNames.includes(card.name)
+    return (
+      <div key={card.name}>
+        <div>
+          <img src={card.image} />
+        </div>
+        {!selected && <button onClick={handleSelect}>Select</button>}
+      </div>
     )
   })
   return (
     <>
       <h1>Tarotarot</h1>
+      {selectedNames.length > 0 && (
+        <>
+          <h2>Selected</h2>
+          <table style={{ marginBottom: '50px' }}>
+            <thead>
+              <tr>
+                <td>Designation</td>
+                <td>Name</td>
+                <td>Upright</td>
+                <td>Reversed</td>
+                <td>Tags</td>
+                <td>Image</td>
+                <td>Actions</td>
+              </tr>
+            </thead>
+            <tbody>
+              {selectedRows}
+            </tbody>
+          </table>
+        </>
+      )}
       <input
         placeholder='search'
         value={query}
@@ -63,6 +138,9 @@ export default function App () {
         <option value=''>All tags</option>
         {tagOptions}
       </select>
+      <div style={{ display: 'flex', flexWrap: 'wrap' }}> 
+        {gridItems}
+      </div>
       <table>
         <thead>
           <tr>
@@ -71,6 +149,8 @@ export default function App () {
             <td>Upright</td>
             <td>Reversed</td>
             <td>Tags</td>
+            <td>Image</td>
+            <td>Actions</td>
           </tr>
         </thead>
         <tbody>
